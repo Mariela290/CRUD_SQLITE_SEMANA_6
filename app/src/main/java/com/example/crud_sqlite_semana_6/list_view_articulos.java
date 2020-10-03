c98ed77092e1d7154f3a0825844694e7e4016007
Mariela290 <marielacastro0798@gmail.com>
2020-10-02 22:29:10 -0300

    Codificacion de la clase list view articulos

--- app/src/main/java/com/example/crud_sqlite_semana_6/list_view_articulos.java
+++ app/src/main/java/com/example/crud_sqlite_semana_6/list_view_articulos.java
@@ -2,13 +2,99 @@ package com.example.crud_sqlite_semana_6;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.SearchView;
+import android.widget.ListView;
+import java.util.ArrayList;
 
 public class list_view_articulos extends AppCompatActivity {
+
+    android.widget.ListView listViewPersonas;
+    ArrayAdapter adaptador;
+    SearchView searchView;
+    ListView ListView;
+    ArrayList<String> list;
+    ArrayAdapter adapter;
+
+    ConexionSQLITE conexion = new ConexionSQLITE(this);
+    Dto datos = new Dto();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_list_view_articulos);
+
+        listViewPersonas= (ListView) findViewById(R.id.listViewPersonas);
+        searchView = (SearchView) findViewById(R.id.searchView);
+
+        adaptador = new ArrayAdapter(this, android.R.layout.simple_list_item_1, conexion.consultaListaArticulos1());
+        listViewPersonas.setAdapter(adaptador);
+
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String s) {
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String s) {
+                String text = s;
+                adaptador.getFilter().filter(text);
+                return false;
+            }
+        });
+
+
+        listViewPersonas.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int pos, long l) {
+
+                String informacion="Código: "+conexion.consultaListaArticulos().get(pos).getCodigo()+"\n";
+                informacion+="Descripcién: "+conexion.consultaListaArticulos().get(pos).getDescripcion()+"\n";
+                informacion+="Precio: "+conexion.consultaListaArticulos().get(pos).getPrecio();
+
+                Dto articulos = conexion.consultaListaArticulos().get(pos);
+                Intent intent = new Intent(list_view_articulos.this, detalle_articulos.class);
+                Bundle bundle = new Bundle();
+                bundle.putSerializable("articulo", articulos);
+                intent.putExtras(bundle);
+                startActivity(intent);
+            }
+        });
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        int id = item.getItemId();
+
+        if (id == R.id.action_Lista_Articulos) {
+            Intent spinnerActivity = new Intent(this, consulta_spinner.class);
+            startActivity(spinnerActivity);
+            return true;
+        } else if (id == R.id.action_Lista_Articulos1) {
+            Intent ListViewActivity = new Intent(this, list_view_articulos.class);
+            startActivity(ListViewActivity);
+            return true;
+        } /*else if (id == R.id.RecycleView) {
+            //Intent ListViewActivity = new Intent(MainActivity.this, consulta_recyclerView.class);
+            //startActivity(ListViewActivity);
+            return true;
+        }*/
+
+        return super.onOptionsItemSelected(item);
+    }
+
 }
